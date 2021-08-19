d2fbb2100c2ec6e90a18ac51319e1d028d5229fc
Manuel Enrique Garcia Neve <77370606+many903@users.noreply.github.com>
2021-08-18 19:44:34 -0500

    Create luces.c

---
+++ luces.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+//un cambio del arreglo de binarios 
+int main(){
+    int a=0,b=0,c=0,d=0;
+    int numeros[] = {a, b, c, d};
+    for(int i = 0 ; i < 4 ; i++ )
+    {
+
+        for( d = 0 ; d < 1 ; d++)
+        {
+            if (d==1){
+                printf("%d", numeros);
+
+                d==0;
+
+                printf("%d", numeros);
+
+            }
+                
+            for( c = 0 ; c < 1 ; c++){
+                if (c==1){
+                    printf("%d", numeros);
+
+                    c==0;
+
+                    printf("%d", numeros);
+
+                }
+
+                for( b = 0 ; b < 1 ; b++){
+                    if (b==1){
+                    printf("%d", numeros);
+
+                    b==0;
+
+                    printf("%d", numeros);
+
+                    }
+                
+                    for( a = 0 ; a < 1 ; a++){
+
+                        if (a==1){
+
+                            printf("%d", numeros);
+
+                            a==0;
+
+                            printf("%d", numeros);
+
+                            }
+                        /*while (numeros[4] = { a , b , c , d})¨{
+                            printf("0 0 0 0"); 
+                        }*/
+                }
+            }
+        }
+    }
+
+return 0;
+}
+}
