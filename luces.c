7351b88982028b5bffa5776dd90f33f58f6b84b7
Manuel Enrique Garcia Neve <77370606+many903@users.noreply.github.com>
2021-08-21 20:53:57 -0500

    Update luces.c

--- luces.c
+++ luces.c
@@ -1,61 +1,39 @@
+/*
+creado por Manuel Enrique Garcia Neve
+fue echa para imprimir en terminal una corrida (0001,0010,0100,1000) de manera ciclica de
+*/
 #include <stdio.h>
-//un cambio del arreglo de binarios 
+void camnbio(int a,int b,int c,int d);//es una funcion que perimite hacer los cambios en a,b,c,d 
 int main(){
-    int a=0,b=0,c=0,d=0;
-    int numeros[] = {a, b, c, d};
-    for(int i = 0 ; i < 4 ; i++ )
-    {
+    int a,b,c,d;
+    a = 0; b = 0 ; c = 0 ; d = 0;
+    camnbio(a,b,c,d);// imboca la fincion declarada al inicio
+    while(1);// esto se coloca para que nunca pare la funcion 
 
-        for( d = 0 ; d < 1 ; d++)
-        {
-            if (d==1){
-                printf("%d", numeros);
+}
 
-                d==0;
+void camnbio(int a,int b,int c,int d)
+{
+    printf("%d,%d,%d,%d",a,b,c,d);
+    if(d==0){
+        printf("%d,%d,%d,%d",&a,&b,&c,&d);
+        d=0;
+        c=1;
+        printf("%d,%d,%d,%d",&a,&b,&c,&d);
 
-                printf("%d", numeros);
-
-            }
-                
-            for( c = 0 ; c < 1 ; c++){
-                if (c==1){
-                    printf("%d", numeros);
-
-                    c==0;
-
-                    printf("%d", numeros);
-
-                }
-
-                for( b = 0 ; b < 1 ; b++){
-                    if (b==1){
-                    printf("%d", numeros);
-
-                    b==0;
-
-                    printf("%d", numeros);
-
-                    }
-                
-                    for( a = 0 ; a < 1 ; a++){
-
-                        if (a==1){
-
-                            printf("%d", numeros);
-
-                            a==0;
-
-                            printf("%d", numeros);
-
-                            }
-                        /*while (numeros[4] = { a , b , c , d})¨{
-                            printf("0 0 0 0"); 
-                        }*/
-                }
-            }
-        }
+    } 
+    if (c==1) {
+        c=0;
+        b=1;
+        printf("%d,%d,%d,%d",&a,&b,&c,&d);
+    }
+    if(b==1){
+        a=1;
+        b=0;
+    }
+    if (a==1){
+    a=0;
+     printf("%d,%d,%d,%d",&a,&b,&c,&d);
     }
 
-return 0;
-}
 }
